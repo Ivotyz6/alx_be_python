0ebc155ed0d72f484262b6b891d19c2236b7a1ec
Ivan Membo <ivotyz@gmail.com>
2025-07-15 07:03:55 +0300

    add python_introduction folder

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#promt user to enter their current age
+current_age = int(input("Enter your current age: "))
+#assume the current year is 2023
+current_year = 2023 
+#calculate the future age in 27 years
+future_age = current_age + 27
+#calculate the year when the user will be 27 years older    
+future_year = current_year + 27
+#print the result       
+print(f"In {future_year}, you will be {future_age} years old.")     
+
