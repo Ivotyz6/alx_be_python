196eb716c41cedca44b0f3cc258e036d47e8d967
Ivan Membo <ivotyz@gmail.com>
2025-07-15 04:53:20 +0300

    add python_introduction folder

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,13 @@
+# Assign specific values to variables
 number1 = 10
 number2 = 5
-print("number1")
-print("number2")
 
-print("number1" + "number2")
-print("number1" - "number2")
-print("number1" * "number2")
+# Perform addition, subtraction and multiplication
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
+# Display the results
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define principal
+principal = 1000  # Principal amount in dollars
+# define rate of interest
+rate = 0.05  # Rate of interest in percent per annum
+# define time period    
+time = 3  # Time period in years
+# calculate simple interest
+simple_interest = (principal * rate * time)    
+# print the result
+print(f"The simple interest for a principal of ${principal}, rate of interest of {rate}, over a period of {time} years is: ${simple_interest}")
