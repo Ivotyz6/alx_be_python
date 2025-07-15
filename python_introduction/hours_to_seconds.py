c4ab5626d3f19c7a2b16f9b6a8402f846f593d91
Ivan Membo <ivotyz@gmail.com>
2025-07-15 06:26:18 +0300

    add python_introduction folder

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+#Define variable hours
+hours = 2
+# Convert hours to seconds
+seconds = hours * 3600  
+# Print the result  
+print(f"{hours} hours is  {seconds} seconds.")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+# define two variables for length and width
+length = 10  # Length of the rectangle in units
+width = 5   # Width of the rectangle in units   
+# calculate area of the rectangle
+area = length * width   
+# print the result
+print(f"The area of the rectangle is: {area}")
+
+
+# rectangle_area.py
