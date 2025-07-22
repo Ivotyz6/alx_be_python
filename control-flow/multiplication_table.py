1cc905c551e395d528453a9f684f51a6e3462285
Ivan Membo <ivotyz@gmail.com>
2025-07-22 06:50:46 +0300

    Add control flow folder

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,4 +3,6 @@ number = int(input("Enter a number to see its multiplication table: "))
 # Generate and print multiplication table
 for i in range(1, 11):
     result = number * i
-    print(f"{number} x {i} = {result}") 
+    print(f"{number} x {i} = {product}")
+    
+     
