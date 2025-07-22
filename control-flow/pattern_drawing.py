edd2b12f9d50110a9c03652128c0a2eb9e487ada
Ivan Membo <ivotyz@gmail.com>
2025-07-22 07:06:43 +0300

    add control-flow folder

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+# prompt user for pattern size
+size = int(input("Enter the size of the pattern: "))
+#Draw the pattern
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end=" ")
+    print()  # Move to the next line after each row
+    row += 1
